980b749d53c215559f2888a8844795eb856ebf6a
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2023-07-18 21:54:33 +0200

    chore: adding latest from frontend

--- tests_generated/test_all_default_parameters_off_starter/frontend/src/main.tsx
+++ tests_generated/test_all_default_parameters_off_starter/frontend/src/main.tsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/App.css'
 import ErrorBoundary from './components/ErrorBoundary'
-import ErrorFallback from './components/ErrorFallback'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ErrorBoundary fallback={<ErrorFallback />}>
+    <ErrorBoundary>
       <App />
     </ErrorBoundary>
   </React.StrictMode>,
